e244b7a22d7dde7e6ea82da9c3afc5b3876078c6
JESSIE XU (ITA-ISDC-ISD-OOCLL/ZHA) <XUJE8@OOCL.COM>
2018-07-13 11:58:57 +0800

    [jessie]extract the method of splitTheInputStr

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -4,22 +4,35 @@ import java.util.*;
  * Created by jxzhong on 2018/5/22.
  */
 public class WordFrequencyGame {
+    public List<Input> splitTheInputStr(String inputStr){
+        String[] arr = inputStr.split("\\s+");
+        List<Input> inputList = new ArrayList<>();
+        for (String s : arr) {
+            Input input = new Input(s, 1);
+            inputList.add(input);
+        }
+        return inputList;
+    }
+
+
     public String getResult(String inputStr) {
 
-        if (inputStr.split("\\s+").length == 1) {
+        if (inputStr.split("\\s+").length == 1) {//如果只有一个词的话
             return inputStr + " 1";
         } else {
 
             try {
 
                 //split the input string with 1 to n pieces of spaces
-                String[] arr = inputStr.split("\\s+");
-
+//                String[] arr = inputStr.split("\\s+");
+//
+//                List<Input> inputList = new ArrayList<>();
+//                for (String s : arr) {
+//                    Input input = new Input(s, 1);
+//                    inputList.add(input);
+//                }
                 List<Input> inputList = new ArrayList<>();
-                for (String s : arr) {
-                    Input input = new Input(s, 1);
-                    inputList.add(input);
-                }
+                inputList = splitTheInputStr(inputStr);
 
                 //get the map for the next step of sizing the same word
                 Map<String, List<Input>> map = getListMap(inputList);
